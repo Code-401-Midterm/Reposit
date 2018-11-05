dc52eebbf8e7afbf6d0b01287184ae21cb86fb0d
Richard J <richjim89@gmail.com>
2018-11-05 14:43:59 -0800

    Models created

---
+++ Reposit/Reposit/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reposit.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        public ICollection<FullSnippet> Snippets { get; set; }
+    }
+}
