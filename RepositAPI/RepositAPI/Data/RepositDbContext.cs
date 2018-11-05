927ca1169e04a0a0c5cba03f3b2078f18f6366d1
ellisken <kse104@gmail.com>
2018-11-05 13:45:28 -0800

    create dbcontext and register

---
+++ RepositAPI/RepositAPI/Data/RepositDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepositAPI.Data
+{
+    public class RepositDbContext : DbContext
+    {
+        public RepositDbContext(DbContextOptions<RepositDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Snippet> Snippets { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<SnippetTag> SnippetTags { get; set; }
+    }
+}
--- RepositAPI/RepositAPI/Startup.cs
+++ RepositAPI/RepositAPI/Startup.cs
@@ -5,17 +5,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RepositAPI.Data;
 
 namespace RepositAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
+            builder.AddUserSecrets<Startup>();
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<RepositDbContext>(options => options.UseSqlServer(Configuration["ProductionDb"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
