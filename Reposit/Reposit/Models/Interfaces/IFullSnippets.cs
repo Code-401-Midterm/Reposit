fe64e4c9fd39b06d1b208de429299480f190558c
Jeremy Beck <36429023+mtncrawler@users.noreply.github.com>
2018-11-05 17:29:30 -0800

    added fullsnippet interface and service for dependency injection

---
+++ Reposit/Reposit/Models/Interfaces/IFullSnippets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reposit.Models.Interfaces
+{
+    public interface IFullSnippets
+    {
+        Task AddSnippet(FullSnippet fullSnippet);
+
+        Task UpdateSnippet(FullSnippet fullSnippet);
+
+        Task DeleteSnippet(FullSnippet fullSnippet);
+
+        Task<List<FullSnippet>> GetSnippets();
+
+        Task<FullSnippet> GetSnippet(int? id);
+    }
+}
---
+++ Reposit/Reposit/Models/Services/FullSnippetService.cs
@@ -0,0 +1,44 @@
+﻿using Reposit.Data;
+using Reposit.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reposit.Models.Services
+{
+    public class FullSnippetService : IFullSnippets
+    {
+        private RepositDbContext _context;
+
+        public FullSnippetService(RepositDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task AddSnippet(FullSnippet fullSnippet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteSnippet(FullSnippet fullSnippet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<FullSnippet> GetSnippet(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<FullSnippet>> GetSnippets()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateSnippet(FullSnippet fullSnippet)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
