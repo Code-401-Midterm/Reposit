edb9be87994e05a7e9b9efadc2982e520d284aba
ellisken <kse104@gmail.com>
2018-11-07 17:50:54 -0800

    change snippetDTO language to string

--- RepositAPI/RepositAPI/Models/SnippetDTO.cs
+++ RepositAPI/RepositAPI/Models/SnippetDTO.cs
@@ -12,7 +12,7 @@ namespace RepositAPI.Models
         public string Title { get; set; }
         public DateTime DateCreated { get; set; }
         public string CodeBody { get; set; }
-        public Language Language { get; set; }
+        public string Language { get; set; }
         public string Notes { get; set; }
         public string Author { get; set; }
         public int AuthorID { get; set; }
