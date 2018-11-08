7f1e7324738462843c1d7a01c0e16c1aa63bfcc6
inkh <jhwe92@gmail.com>
2018-11-08 15:29:44 -0800

    copy to clipboard fully functional

--- Reposit/Reposit/wwwroot/app.js
+++ Reposit/Reposit/wwwroot/app.js
@@ -1,24 +1,30 @@
 ﻿(function ($) {
     "use strict"; // Start of use strict
 
+    //Hides raw snippet
+    $(".snip").hide();
+
     //Event listener for copy
-    $("#copyButton").on("click", function () {
+    $(".copyButton").on("click", function () {
         copyToClipboard();
     });
 
     //Copy to clipboard
     function copyToClipboard() {
-        //Text body
-        //let $textToCopy = $('#snip');
-        let $textToCopy = $('#snip');
+        //Text body. Shows raw snippet briefly until selection and copy is completed
+        $(".snip").show();
+        var $textToCopy = $('.snip');
 
         //Select the text body
         $textToCopy.select();
 
         //Copy the text
         document.execCommand("copy");
-        //debugger;
+
+        //Hide text again
+        $(".snip").hide();
     }
+
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
