1e8f9d63151181c7798daf63607b70b1cc129479
ellisken <kse104@gmail.com>
2018-11-07 19:57:06 -0800

    fix select for create snippet

--- Reposit/Reposit/Migrations/20181108034029_changes.Designer.cs
+++ Reposit/Reposit/Migrations/20181108034029_changes.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Reposit.Data;
 
 namespace Reposit.Migrations
 {
     [DbContext(typeof(RepositDbContext))]
-    partial class RepositDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181108034029_changes")]
+    partial class changes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
